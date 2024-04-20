9cb7d7ed7b80d39ef6f190bf1f97ff01c469bbd2
Sai Prakash <coddinginjava@gmail.com>
2024-04-20 09:00:07 +0530

    implemented Completed feature

--- src/components/Notes/NotesModule.jsx
+++ src/components/Notes/NotesModule.jsx
@@ -1,8 +1,8 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useMemo, useRef, useState} from "react";
 import Notes from "./Notes.jsx";
 import {getTodosBasedOnUserId, deleteTodo, addTodo, updateTodo} from "../../firebase-config/firestore.js"
 import {useAuth} from "../../context/AuthContext.jsx"
-import {Button, Container, Form, InputGroup} from "react-bootstrap";
+import {Accordion, Button, Container, Form, InputGroup} from "react-bootstrap";
 import {v4 as uuidv4} from 'uuid'
 
 
@@ -11,13 +11,31 @@ const NotesModule = () => {
     const {currentUser} = useAuth()
 
     const [notes, setNotes] = useState([]);
+    const [pendingNotes, setPendingNotes] = useState([]);
+    const [completedNotes, setCompletedNotes] = useState([]);
     const [newNotes, setNewNotes] = useState("");
 
+    const handleStateChange = (givenNotes) => {
+        let pending = [];
+        let completed = [];
+        givenNotes.map(currentNote => {
+            if (currentNote.completed) {
+                completed.push(currentNote);
+            } else {
+                pending.push(currentNote);
+            }
+        })
+
+        setPendingNotes(pending);
+        setCompletedNotes(completed);
+    }
 
     const handleDeleteNote = (id) => {
-        deleteTodo(id).then(() => setNotes(pn => pn.filter(n => n.id !== id)));
-        // deleteTodo(id).then(() => console.log("deleted successfully id : " + id)).catch(e => console.log("deleted error: " + e))
-        // setNotes(pn => pn.filter(n => n.id !== id));
+        deleteTodo(id).then(() => setNotes(pn => {
+            const filteredNotes = pn.filter(n => n.id !== id)
+            handleStateChange(filteredNotes);
+            return filteredNotes;
+        }));
     }
 
     const handleUpdateNotes = ({id, completed}) => {
@@ -26,26 +44,32 @@ const NotesModule = () => {
             return
         filterNote[0].completed = completed;
 
-        // updateTodo(filterNote[0])
-        //     .then(() => console.log("Updates notes with id: " + id))
-        //     .catch(e => console.error(e));
-        updateTodo(filterNote[0]).then(() => setNotes(pn => pn.map(n => {
-            if(n===id)
-                return {...n, completed: completed};
-            else return n;
-        })));
-        setNotes(pn => pn.map(n => {
-            if (n === id)
-                return {...n, completed: completed};
-            else return n;
-        }));
 
+        // if (Date.now() - filterNote[0].createdOn < 3000) {
+        //     setTimeout(() => {
+        //         updateTodofucntion()
+        //     }, 2000);
+        // }
+
+
+            updateTodo(filterNote[0]).then(() => setNotes(pn => {
+                const tempUpdatedNotes = pn.map(n => {
+                    if (n === id)
+                        return {...n, completed: completed};
+                    else return n;
+                });
+                handleStateChange(tempUpdatedNotes);
+                return tempUpdatedNotes;
+            }));
     }
+
+
     useEffect(() => {
         async function fetchNotes() {
-            const asdasdasd = await getTodosBasedOnUserId(currentUser?.uid);
-            console.log({asdasdasd})
-            setNotes(asdasdasd)
+            const userBasedNotes = await getTodosBasedOnUserId(currentUser?.uid);
+            console.log({userBasedNotes})
+            setNotes(userBasedNotes)
+            handleStateChange(userBasedNotes)
         }
 
         fetchNotes()
@@ -53,20 +77,23 @@ const NotesModule = () => {
 
 
     const handleAddNewNote = (value) => {
-        if (!value)
+        const val = value.trim()
+        if (!val)
             return;
         const newNoteBody = {
-            content: value,
+            content: val,
             completed: false,
             user_uid: currentUser?.uid,
             createdOn: new Date(),
             id: uuidv4(),
 
         }
-        addTodo(newNoteBody).then(() => setNotes(prev => [...prev, newNoteBody]));
-        // addTodo(newNoteBody).then(() => console.log("added successfully with id : " + newNoteBody.id)).catch(e => console.error(e));
-        // setNotes(prev => [...prev, newNoteBody])
+        addTodo(newNoteBody).then(() => setNotes(prev => {
+            handleStateChange([...prev, newNoteBody]);
+            return [...prev, newNoteBody]
+        }));
         setNewNotes("")
+
     }
 
     return (
@@ -77,6 +104,7 @@ const NotesModule = () => {
                 <div>
                     <InputGroup size="lg" variant="dark">
                         <Button variant="outline-secondary" onClick={() => handleAddNewNote(newNotes)}
+                                disabled={newNotes.trim().length === 0}
                                 id="button-addon1">
                             Add Note
                         </Button>
@@ -89,9 +117,24 @@ const NotesModule = () => {
                         />
                     </InputGroup>
                 </div>
-                <div className="w-100 mt-5" style={{maxWidth: "600px"}}>
-                    <Notes notes={notes} deleteNote={handleDeleteNote} updateNotes={handleUpdateNotes}/>
-                </div>
+                {notes?.length > 0 && <div className="w-100 mt-5" style={{maxWidth: "600px"}}>
+                    <Accordion defaultActiveKey={['0']}>
+                        {pendingNotes?.length > 0 && <Accordion.Item eventKey="0">
+                            <Accordion.Header>Pending Tasks</Accordion.Header>
+                            <Accordion.Body>
+                                <Notes notes={pendingNotes} deleteNote={handleDeleteNote}
+                                       updateNotes={handleUpdateNotes}/>
+                            </Accordion.Body>
+                        </Accordion.Item>}
+                        {completedNotes.length > 0 && <Accordion.Item eventKey="1">
+                            <Accordion.Header>Completed Tasks</Accordion.Header>
+                            <Accordion.Body>
+                                <Notes notes={completedNotes} deleteNote={handleDeleteNote}
+                                       updateNotes={handleUpdateNotes}/>
+                            </Accordion.Body>
+                        </Accordion.Item>}
+                    </Accordion>
+                </div>}
             </Container>
         </>
     )
